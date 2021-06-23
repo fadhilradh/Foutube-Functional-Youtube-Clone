6b6f13d7522fc0da8df8c08509f24873a71cbcfc
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-24 03:36:23 +0700

    [ADD] redux

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaJLduF3iRLPWPspylFBlicSb8ApPxzgY",
+  authDomain: "foutube-react.firebaseapp.com",
+  projectId: "foutube-react",
+  storageBucket: "foutube-react.appspot.com",
+  messagingSenderId: "138528611228",
+  appId: "1:138528611228:web:c075dc895a868b53134659",
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./_base.scss";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
