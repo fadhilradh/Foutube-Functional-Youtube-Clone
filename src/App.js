df7dcbcc47a4d86782cba6dbe60b629009e97667
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-24 03:18:29 +0700

    [ADD] login screen & router

--- src/App.js
+++ src/App.js
@@ -2,24 +2,59 @@ import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screen/HomeScreen";
+import LoginScreen from "./screen/loginScreen/loginScreen";
 import "./_app.scss";
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
+const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(false);
 
   const handleToggleSidebar = () => toggleSidebar((value) => !value);
 
   return (
-    <div>
+    <>
       <Header handleToggleSidebar={handleToggleSidebar} />
       <div className="app__container ">
         <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
         <Container fluid className="app__main ">
-          <HomeScreen />
+          {children}
         </Container>
       </div>
-    </div>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Layout>
+            <HomeScreen />
+          </Layout>
+        </Route>
+
+        <Route path="/auth">
+          <LoginScreen />
+        </Route>
+
+        <Route path="/search">
+          <Layout>
+            <h1>Search Results</h1>
+          </Layout>
+        </Route>
+
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/screen/loginScreen/loginScreen.js
+++ src/screen/loginScreen/loginScreen.js
@@ -4,6 +4,7 @@ const LoginScreen = () => {
   return (
     <div className="login">
       <div className="login__container">
+        <h1>Youtube Clone</h1>
         <img src="/blue-logo.jpeg" alt="" />
         <button>Login with Google</button>
         <p>Made by Fadhil Radhian using React, Redux and Youtube API</p>
