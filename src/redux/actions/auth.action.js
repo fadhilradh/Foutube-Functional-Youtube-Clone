94014ba7c6ae7e0a1ef3b6c8bc4c0b8ac83f9028
Fadhil Radhian <fadhilradhian@gmail.com>
2021-07-10 19:23:06 +0700

    update action

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,34 +1,46 @@
 import auth from "../../firebase";
 import firebase from "firebase";
-import { LOGIN_REQUEST, LOGIN_SUCCESS } from "../actionTypes";
+import {
+    LOAD_PROFILE,
+    LOGIN_FAILED,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+} from "../actionTypes";
 
-export const loginWithGoogle = async (dispatch) => {
-  // auth with Google
+export const loginWithGoogle = () => async (dispatch) => {
+    // auth with Google
+    try {
+        dispatch({
+            type: LOGIN_REQUEST,
+        });
 
-  dispatch({
-    type: LOGIN_REQUEST,
-  });
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-  const googleProvider = new firebase.auth.GoogleAuthProvider();
+        const response = await auth.signInWithPopup(googleProvider);
 
-  const response = await auth.signInWithPopup(googleProvider);
+        console.log(response);
 
-  console.log(response);
+        const accessToken = response.credential.accessToken;
 
-  const accessToken = response.credential.accessToken;
+        const profile = {
+            name: response.additionalUserInfo.profile.name,
+            photo: response.additionalUserInfo.profile?.picture,
+        };
 
-  const profile = {
-    name: response.additionalUserInfo.profile.displayName,
-    photo: response.additionalUserInfo.profile?.photoURL,
-  };
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: accessToken,
+        });
 
-  dispatch({
-    type: LOGIN_SUCCESS,
-    payload: accessToken,
-  });
-
-  try {
-  } catch (error) {
-    console.log(error);
-  }
+        dispatch({
+            type: LOAD_PROFILE,
+            payload: profile,
+        });
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: LOGIN_FAILED,
+            payload: error.message,
+        });
+    }
 };
